02a48e584367c2f0c5f88c68bc63ec52f9de89bf
Cauê Faleiros <cauefaleiros@gmail.com>
2024-05-23 14:15:07 -0300

    Inicio de adição de tratamento de excessões

--- api/src/main/java/br/com/api/helpdesk/services/PatrimonioService.java
+++ api/src/main/java/br/com/api/helpdesk/services/PatrimonioService.java
@@ -1,20 +1,10 @@
 package br.com.api.helpdesk.services;
 
 import br.com.api.helpdesk.exception.IdPatrimonioBadRequest;
-import br.com.api.helpdesk.exception.PatrimonioNotFound;
-import br.com.api.helpdesk.infra.RestErrorMessage;
-import br.com.api.helpdesk.infra.RestExceptionHandler;
 import br.com.api.helpdesk.models.PatrimonioModel;
 import br.com.api.helpdesk.repositories.PatrimonioRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.List;
 import java.util.Optional;
