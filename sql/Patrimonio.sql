7b995c7fa758323c61055e3392cd2ca4246ae055
Cauê Faleiros <125504745+cauefaleiros@users.noreply.github.com>
2024-05-24 11:37:02 -0300

    Update Patrimonio.sql

--- sql/Patrimonio.sql
+++ sql/Patrimonio.sql
@@ -5,14 +5,14 @@ INSERT INTO patrimonio(id_patrimonio,data_criacao, descricao, nome_patrimonio, p
 -- PATRIMONIOS CRIADOS:
 
 INSERT INTO patrimonio(id_patrimonio,data_criacao, descricao, nome_patrimonio, preco, quantidade) VALUES
-    (gen_random_uuid(),'2024-05-24T12:26:14.447+00:00','GIFT CARD XBOX 100 REAIS', 'Gift card Games Pass', 100.00, 24);
+    ('11007eb7-ee2d-41e5-b349-18b8e33f040f','2024-05-24T12:26:14.447+00:00','GIFT CARD XBOX 100 REAIS', 'Gift card Games Pass', 100.00, 24);
 
 INSERT INTO patrimonio(id_patrimonio,data_criacao, descricao, nome_patrimonio, preco, quantidade) VALUES
-    (gen_random_uuid(),'2024-05-24T12:37:14.447+00:00','GIFT CARD PLAYSTATION 300 REAIS', 'Gift card Playstation', 300.00, 42);
+    ('3c79be4d-bc95-4600-9faa-b67f1d98522d','2024-05-24T12:37:14.447+00:00','GIFT CARD PLAYSTATION 300 REAIS', 'Gift card Playstation', 300.00, 42);
 
 INSERT INTO patrimonio(id_patrimonio,data_criacao, descricao, nome_patrimonio, preco, quantidade) VALUES
-    (gen_random_uuid(),'2024-05-24T12:26:14.447+00:00','GIFT CARD NINTENDO 500 REAIS', 'Gift card Nintendo', 500.00, 9);
+    ('9933e778-5fcc-4b76-ba6b-cd05aa590356','2024-05-24T12:26:14.447+00:00','GIFT CARD NINTENDO 500 REAIS', 'Gift card Nintendo', 500.00, 9);
 
 INSERT INTO patrimonio(id_patrimonio,data_criacao, descricao, nome_patrimonio, preco, quantidade) VALUES
-    (gen_random_uuid(),'2024-05-24T12:26:14.447+00:00' ,'GIFT CARD IFOOD 50 REAIS', 'Gift card Ifood', 50.00, 123);
+    ('8a4be5dc-328c-4edc-9f94-3554b7a07a54','2024-05-24T12:26:14.447+00:00' ,'GIFT CARD IFOOD 50 REAIS', 'Gift card Ifood', 50.00, 123);
 
